b0d639d5c6080078b08d3b1d3a1bff222eceeefc
Connor Campbell <connorcam123@gmail.com>
2024-01-30 17:30:34 +0000

    Removed vercel build

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/_layout.server.ts.js
+++
@@ -1,8 +0,0 @@
-import { g as getPlayers } from "../../chunks/db-functions.js";
-const load = async ({ url }) => {
-  const playerList = await getPlayers();
-  return { playerList, url: url.pathname };
-};
-export {
-  load
-};
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1._Ow80i6b.js","_app/immutable/chunks/scheduler.xVbf0__5.js","_app/immutable/chunks/index.YL0iXnvq.js","_app/immutable/chunks/stores.SeQSodgb.js","_app/immutable/chunks/singletons.EzEmGe6U.js"];
-export const stylesheets = [];
-export const fonts = [];
